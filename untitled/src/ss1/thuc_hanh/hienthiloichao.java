9d966cf1d75d60e56b4f850c1b0509290820ef1b
Your Name <you@example.com>
2024-11-30 21:33:06 +0700

    first commit

--- untitled/src/ss1/thuc_hanh/hienthiloichao.java
+++ untitled/src/ss1/thuc_hanh/hienthiloichao.java
@@ -2,7 +2,7 @@ package ss1.thuc_hanh;
 
 import java.util.Scanner;
 
-public class demo1 {
+public class hienthiloichao {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập tên của bạn: ");
--- untitled/src/ss1/thuc_hanh/ungdungdoitiente.java
+++ untitled/src/ss1/thuc_hanh/ungdungdoitiente.java
@@ -1,4 +1,24 @@
 package ss1.thuc_hanh;
 
-public class Ungdungdoitiente {
+import java.util.Scanner;
+
+public class ungdungdoitiente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 đổi từ VND sang USD: ");
+        System.out.println("2 đổi từ USD sang VND ");
+        int choi = sc.nextInt();
+        if (choi == 1) {
+            System.out.println("Nhập số tiền: ");
+            int VND = sc.nextInt();
+            double doi = VND * 25.3;
+            System.out.println("1 đổi từ USD sang VND: " + doi + " vnd");
+        }else if (choi == 2) {
+            System.out.println("Nhập số tiền: ");
+            int USD = sc.nextInt();
+            double doi = USD / 25.3;
+            System.out.println("2 đổi từ USD sang VND: " + doi+ " usd");
+
+        }
+    }
 }
